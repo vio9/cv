edc753ec1867f77d4080bcc000071de510d8bd0c
violaine <e.violaine@yahoo.fr>
2022-01-18 20:41:20 +0100

    change link

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -3,7 +3,7 @@ import React from "react";
 const Presentation = () => {
 	return (
 		<div className="presentation">
-			<h2>Bienvenue sur mon CV en ligne !</h2>
+			<h2>Bienvenue sur mon CV en ligne ! ✨</h2>
 			<p>
 				Retrouvez ci-dessous mes compétences, expériences professionnelles et
 				formation. Pour me contacter, n'hésitez pas à cliquer sur la page
