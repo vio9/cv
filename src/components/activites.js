76c31ed63c084f9f873e058b2f6e561ea518c99b
violaine <e.violaine@yahoo.fr>
2022-01-24 18:29:51 +0100

    mise  ajour activité

--- src/components/activites.js
+++ src/components/activites.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Header";
 import Navigation from "./Navigation";
 
 const Activites = () => {
 	return (
 		<div>
+			<Header />
 			<Navigation />
 		</div>
 	);
