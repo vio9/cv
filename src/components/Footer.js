0d0e34346bec36994d729395666f6c78e32126e6
violaine <e.violaine@yahoo.fr>
2022-01-25 14:43:23 +0100

    footer fix scss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,39 +6,33 @@ import linkedin from "../datas/assets/linkedin.png";
 
 const Footer = () => {
 	return (
-		<footer>
-			<div className="footer">
-				<NavLink className="footer-link" exact to="/">
-					Home
-				</NavLink>
-				<NavLink className="footer-link" to="Contact">
-					Contact
-				</NavLink>
-				<NavLink className="footer-link" to="Activites">
-					Activités
-				</NavLink>
-				<NavLink className="footer-link" to="Activites">
-					Activités
-				</NavLink>
-				<a href="https://github.com/vio9" target="_blank">
-					<img
-						className="footer-link-image"
-						src={githubwhite}
-						alt="github-logo"
-					/>
-				</a>
-				<a href="https://gitlab.com/violaine09" target="_blank">
-					<img className="footer-link-image" src={gitlab} alt="gitlab-logo" />
-				</a>
-				<a href="https://www.linkedin.com/in/violaine-ernotte/" target="_blank">
-					<img
-						className="footer-link-image"
-						src={linkedin}
-						alt="linkedin-logo"
-					/>
-				</a>
-			</div>
-		</footer>
+		<div className="footer">
+			<NavLink className="footer-link" exact to="/">
+				Home
+			</NavLink>
+			<NavLink className="footer-link" to="Contact">
+				Contact
+			</NavLink>
+			<NavLink className="footer-link" to="Activites">
+				Activités
+			</NavLink>
+			<NavLink className="footer-link" to="Activites">
+				Activités
+			</NavLink>
+			<a href="https://github.com/vio9" target="_blank">
+				<img
+					className="footer-link-image"
+					src={githubwhite}
+					alt="github-logo"
+				/>
+			</a>
+			<a href="https://gitlab.com/violaine09" target="_blank">
+				<img className="footer-link-image" src={gitlab} alt="gitlab-logo" />
+			</a>
+			<a href="https://www.linkedin.com/in/violaine-ernotte/" target="_blank">
+				<img className="footer-link-image" src={linkedin} alt="linkedin-logo" />
+			</a>
+		</div>
 	);
 };
 
